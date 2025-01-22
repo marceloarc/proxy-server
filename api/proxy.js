d6c88ef2fc92adcae50807c8d05644822d0ce648
Marcelo Augusto <59924047+marceloarc@users.noreply.github.com>
2025-01-22 20:18:14 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -7,10 +7,9 @@ export default async function handler(req, res) {
   // Verifique se a requisição é do tipo OPTIONS (preflight request)
   if (req.method === 'OPTIONS') {
     // Responda aos cabeçalhos de CORS para a preflight request (OPTIONS)
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    re.setHeader("Access-Control-Allow-Credentials", "true");
-    re.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    re.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     return res.status(200).end(); // Retorne um OK para a preflight request
   }
 
@@ -28,10 +27,9 @@ export default async function handler(req, res) {
     const data = await response.text(); // Obtendo a resposta
 
     // Adicionando os cabeçalhos de CORS
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
     // Retornando a resposta da requisição para o frontend
     res.status(response.status).send(data);
