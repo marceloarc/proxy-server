66a267507d80e7088def61594ec1e8e9b065d87d
Marcelo Augusto <59924047+marceloarc@users.noreply.github.com>
2025-01-22 21:00:42 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,30 +1,29 @@
 const allowCors = (fn) => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');  // Ou personalize, por exemplo, com `req.headers.origin`
+  res.setHeader('Access-Control-Allow-Origin', '*');  // Altere se necessário (teste `*` para "qualquer origem" ou seu domínio específico).
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
   
   if (req.method === 'OPTIONS') {
-    res.status(200).end();  // Pre-flight response
+    res.status(200).end();  // Isso responde à requisição do tipo "preflight"
     return;
   }
   
-  // Chama a função de tratamento da requisição
-  return await fn(req, res);
+  return await fn(req, res);  // Chama a função de processamento
 };
 
 const handler = async (req, res) => {
-  const url = req.query.url;
+  const url = req.query.url; // A URL que você está pedindo para acessar via proxy
 
-  // Realiza a chamada para o URL solicitado via proxy
   try {
+    // Fez o proxy da chamada de API para o destino
     const response = await fetch(url);
     const data = await response.json();
-
-    res.status(200).json(data);  // Retorna a resposta da API solicitada
+    
+    res.status(200).json(data);  // Retorna os dados da resposta da API
   } catch (error) {
     res.status(500).json({ error: 'Error fetching data from URL' });
   }
 };
 
-module.exports = allowCors(handler);
+module.exports = allowCors(handler);  // Exporta o proxy com o CORS
